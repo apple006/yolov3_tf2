08ea2ae5afd21cd66432791fb29a46895b66211a
xxxxxx <xxxx@.com>
2019-05-01 15:33:11 +0800

    add

--- tests.py
+++ tests.py
@@ -1,16 +1,10 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-# tfds works in both Eager and Graph modes
-tf.enable_eager_execution()
 
-# See available datasets
-print(tfds.list_builders())
+dataset, metadata = tfds.load('fashion_mnist', as_supervised=True, with_info=True)
+train_dataset, test_dataset = dataset['train'], dataset['test']
 
-# Construct a tf.data.Dataset
-dataset = tfds.load(name="mnist", split=tfds.Split.TRAIN)
 
-# Build your input pipeline
-dataset = dataset.shuffle(1024).batch(32).prefetch(tf.data.experimental.AUTOTUNE)
-for features in dataset.take(1):
-  image, label = features["image"], features["label"]
+print(train_dataset)
+print(test_dataset)
