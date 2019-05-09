6a821e2fc0acd7da928605a946422db67cfff84e
Your Name <you@example.com>
2019-05-09 16:30:02 +0800

    saving weights and fix something

--- train.py
+++ train.py
@@ -1,3 +1,5 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
 import tensorflow as tf
 import numpy as np
 import cv2
@@ -5,18 +7,18 @@ from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping, ModelCh
 from absl import app, flags
 from absl.flags import FLAGS
 from alfred.utils.log import logger as logging
-import os
 
 from yolov3.models import *
 from yolov3.utils import freeze_all
 import dataset.dataset_coco as dataset
 
 
-flags.DEFINE_string('dataset', '/media/jintain/sg/permanent/datasets/coco/*.tfrecord',
+flags.DEFINE_string('dataset', '/media/jintain/wd/permenant/datasets/coco/*.tfrecord',
                     'dataset tfrecor path')
 flags.DEFINE_string('val_dataset', '', 'path to validation dataset')
 flags.DEFINE_boolean('tiny', False, 'yolov3 or yolov3-tiny')
-flags.DEFINE_string('weights', './checkpoints/yolov3_coco.ckpt-{}',
+# actually, both model architecture and weights will be saved
+flags.DEFINE_string('weights', './checkpoints/yolov3_coco-{}.ckpt',
                     'path to weights file')
 flags.DEFINE_string('classes', './data/coco.names', 'path to classes file')
 flags.DEFINE_enum(
@@ -32,7 +34,7 @@ flags.DEFINE_enum(
     'frozen: Transfer and freeze all, '
     'fine_tune: Transfer all and freeze darknet only')
 flags.DEFINE_integer('size', 416, 'image size')
-flags.DEFINE_integer('epochs', 2, 'number of epochs')
+flags.DEFINE_integer('epochs', 102, 'number of epochs')
 flags.DEFINE_integer('batch_size', 8, 'batch size')
 flags.DEFINE_integer('val_per_epoch', 32, 'val_per_epoch')
 flags.DEFINE_float('learning_rate', 1e-3, 'learning rate')
@@ -52,9 +54,13 @@ def main(_argv):
         anchor_masks = yolo_anchor_masks
 
     train_dataset = dataset.load_fake_dataset()
-    if FLAGS.dataset:
+    if FLAGS.dataset and os.path.exists(os.path.dirname(FLAGS.dataset)):
+        logging.info(f'loading dataset from: {FLAGS.dataset}')
         train_dataset = dataset.load_tfrecord_dataset(FLAGS.dataset,
                                                       FLAGS.classes)
+    else:
+        logging.info('{} can not found, did you changed to your machine path?'.format(FLAGS.dataset))
+        exit(0)
     train_dataset = train_dataset.shuffle(buffer_size=1024)  # TODO: not 1024
     train_dataset = train_dataset.batch(FLAGS.batch_size)
     train_dataset = train_dataset.map(lambda x, y: (dataset.transform_images(
@@ -130,9 +136,9 @@ def main(_argv):
                     avg_loss.update_state(total_loss)
                     
                     if batch % 10 == 0:
-                        logging.info("Epoch: {}, iter: {}, total_loss: {}, pred_loss: {}".format(
+                        logging.info("Epoch: {}, iter: {}, total_loss: {:.4f}, pred_loss: {}".format(
                         epoch, batch, total_loss.numpy(), list(map(lambda x: np.sum(x.numpy()), pred_loss))))
-                    if batch % 500 == 0:
+                    if batch % 500 == 0 and batch != 0:
                         logging.info('save model periodically...')
                         model.save_weights(FLAGS.weights.format(epoch))
                 except KeyboardInterrupt:
@@ -156,16 +162,18 @@ def main(_argv):
                         epoch, batch, total_loss.numpy(),
                         list(map(lambda x: np.sum(x.numpy()), pred_loss))))
                     avg_val_loss.update_state(total_loss)
-            logging.info("{}, train: {}, val: {}".format(
-                epoch,
-                avg_loss.result().numpy(),
-                avg_val_loss.result().numpy()))
+                logging.info("{}, train: {}, val: {}".format(
+                    epoch,
+                    avg_loss.result().numpy(),
+                    avg_val_loss.result().numpy()))
 
-            avg_loss.reset_states()
-            avg_val_loss.reset_states()
-            model.save_weights(FLAGS.weights.format(epoch))
-            logging.info('training done.')
-            exit(0)
+                avg_loss.reset_states()
+                avg_val_loss.reset_states()
+        model.save_weights(FLAGS.weights.format(epoch))
+        # save final model (both arcchitecture and weights)
+        model.save('yolov2_coco.h5')
+        logging.info('training done.')
+        exit(0)
     else:
         model.compile(optimizer=optimizer,
                       loss=loss,
